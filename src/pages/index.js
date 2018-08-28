91d3a4c6c5b420811ddd476de8bbe0f9009e3c9e
Mary Chew <jiayi1172@gmail.com>
2018-08-28 13:21:48 +0800

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>I am Mary Chew Jia Yi from Malaysia.</p>
+    <p>I have been coding for quite a while and I love join Hackathons. I can't wait to join this FCC Hackathon.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
